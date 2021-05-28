1e57029d3a866d8d9bc2237bf5ce8d15ed94b2d2
Carlos A <soradev4@gmail.com>
2021-05-28 16:48:27 -0700

    Rescript Setup (#1)
    * init
    
    * updated example

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,15 +1,17 @@
 import { useRecoilValue } from 'recoil';
-import { randomItemState } from '../lib/recoil-atoms';
+import { randomItemState } from '../library/recoil-atoms';
+import { make as ColorComponent } from '../library/Demo/ColorComponent.gen';
 
 
 const IndexPage = () => {
-  const randomItem = useRecoilValue(randomItemState)
+  const randomItem = useRecoilValue(randomItemState);
   return (
     <div>
       <h1>Hello Next.js 👋</h1>
       <p>
         {randomItem}
       </p>
+      <ColorComponent color="Blue" name="Shashan" />
     </div>
   )
 }
