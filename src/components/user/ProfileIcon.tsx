58119c88999eda6a5fa87c13a9b4bef75cdd6e04
Lucemans <luc@lucemans.nl>
2021-05-30 18:04:49 +0200

    Introduce ProfilePopup and Logout (#10)

--- src/components/user/ProfileIcon.tsx
+++ src/components/user/ProfileIcon.tsx
@@ -1,14 +1,20 @@
 import { useQuery } from '@apollo/client';
 import gql from 'graphql-tag';
-import React, { FC } from 'react';
+import React, { FC, useState } from 'react';
 import styled from 'styled-components';
 import { User } from '../../types/user';
+import { ProfilePopup } from './ProfilePopup';
+
+const Wrapper = styled.div`
+    position: relative;
+`;
 
 const Icon = styled.img`
     width: 4rem;
     height: 4rem;
     border-radius: 50%;
     background: ${({ theme }) => theme.palette.primary[700]};
+    cursor: pointer;
 `;
 
 type PROFILE_ICON_TYPE = {
@@ -30,14 +36,22 @@ export const ProfileIcon: FC = () => {
         PROFILE_ICON_QUERY,
         { fetchPolicy: "network-only" }
     );
+    const [expanded, setExpanded] = useState(false);
 
     if (loading || error) {
         return (
-            <Icon />
+            <Wrapper>
+                <Icon />
+            </Wrapper>
         );
     }
 
     return (
-        <Icon src={data.me.avatar} />
+        <Wrapper>
+            <Icon src={data.me.avatar} onClick={() => {
+                setExpanded(!expanded);
+            }} />
+            {expanded && <ProfilePopup />}
+        </Wrapper>
     );
 };
