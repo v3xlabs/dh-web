6adc3fb4161b883f574eb8dbcface40252686720
Lucemans <luc@lucemans.nl>
2021-06-01 23:52:43 +0200

    GraphQL Friends (#58)
    * Auto stash before checking out "origin/master"
    
    * GraphQL, Friendslist, and Types

---
+++ src/__generated__/FriendsQuery.ts
@@ -0,0 +1,34 @@
+/* tslint:disable */
+/* eslint-disable */
+// @generated
+// This file was automatically generated and should not be edited.
+
+// ====================================================
+// GraphQL query operation: FriendsQuery
+// ====================================================
+
+export interface FriendsQuery_me_following_following_current_room {
+  __typename: "Room";
+  name: string;
+}
+
+export interface FriendsQuery_me_following_following {
+  __typename: "User";
+  avatar: string;
+  username: string;
+  current_room: FriendsQuery_me_following_following_current_room | null;
+}
+
+export interface FriendsQuery_me_following {
+  __typename: "Follow";
+  following: FriendsQuery_me_following_following | null;
+}
+
+export interface FriendsQuery_me {
+  __typename: "User";
+  following: FriendsQuery_me_following[];
+}
+
+export interface FriendsQuery {
+  me: FriendsQuery_me;
+}
