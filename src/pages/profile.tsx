0764be4e525dc351b70ea2dbfaed2f2cc3a77d22
Ankit <4nkitd@gmail.com>
2021-06-01 18:05:13 +0530

    added seo links

--- src/pages/profile.tsx
+++ src/pages/profile.tsx
@@ -1,14 +1,61 @@
-import Link from "next/link";
+import useMediaQuery from "@material-ui/core/useMediaQuery";
+import { NextSeo } from "next-seo";
+import React from "react";
 import { FC } from "react";
 
 const Dashboard: FC = () => {
     console.log("Loading dashboard");
 
     return (
-        <div>
-            Profile Page
-            <Link href="/dashboard">Dashboard</Link>
-        </div>
+        <>
+            <NextSeo
+                defaultTitle="Dogehouse Revived"
+                title="Dogehouse Revived | Profile"
+                description="Taking voice conversations to the moon 🚀"
+                canonical="https://www.dogehouse.online/"
+                additionalLinkTags={[
+                    {
+                        rel: "icon",
+                        href: "https://cdn.lvk.sh/dogehouse/logo.svg",
+                    },
+                    {
+                        rel: "apple-touch-icon",
+                        href: "https://cdn.lvk.sh/dogehouse/logo.svg",
+                        sizes: "76x76"
+                    }
+                ]}
+            />
+            <Grid>
+                {
+                    one &&
+                    <Column>
+                        <Logo small={!three} />
+                        <FriendsList />
+                    </Column>
+                }
+                <Column>
+                    <SearchWrapper>
+                        <Search />
+                        {!two && <ProfileIcon />}
+                    </SearchWrapper>
+                    <div>
+                        <ProfileWidgetDataContainer />
+                    </div>
+                </Column>
+                {
+                    two &&
+                    <Column>
+                        <ProfileWrapper>
+                            <ProfileIcon />
+                        </ProfileWrapper>
+                        <div>
+                            <ProfileWidgetDataContainer />
+                            <Schedule />
+                        </div>
+                    </Column>
+                }
+            </Grid >
+        </>
     );
 };
 
