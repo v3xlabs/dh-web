a27c7ec69d604981e393e6c1a2e68239efee5d10
Ankit <4nkitd@gmail.com>
2021-06-01 16:18:35 +0530

    added seo links

--- src/pages/profile.tsx
+++ src/pages/profile.tsx
@@ -1,4 +1,6 @@
 import useMediaQuery from "@material-ui/core/useMediaQuery";
+import { NextSeo } from "next-seo";
+import React from "react";
 import { FC } from "react";
 import styled, { useTheme } from "styled-components";
 
@@ -37,36 +39,55 @@ const Profile: FC = () => {
     const three = useMediaQuery(`(min-width:${theme.breakpoints.three + 1}px)`);
 
     return (
-        <Grid>
-            {
-                one &&
+        <>
+            <NextSeo
+                defaultTitle="Dogehouse Revived"
+                title="Dogehouse Revived | Profile"
+                description="Taking voice conversations to the moon 🚀"
+                canonical="https://www.dogehouse.online/"
+                additionalLinkTags={[
+                    {
+                        rel: "icon",
+                        href: "https://cdn.lvk.sh/dogehouse/logo.svg",
+                    },
+                    {
+                        rel: "apple-touch-icon",
+                        href: "https://cdn.lvk.sh/dogehouse/logo.svg",
+                        sizes: "76x76"
+                    }
+                ]}
+            />
+            <Grid>
+                {
+                    one &&
+                    <Column>
+                        <Logo small={!three} />
+                        <FriendsList />
+                    </Column>
+                }
                 <Column>
-                    <Logo small={!three} />
-                    <FriendsList />
-                </Column>
-            }
-            <Column>
-                <SearchWrapper>
-                    <Search />
-                    {!two && <ProfileIcon />}
-                </SearchWrapper>
-                <div>
-                    <ProfileWidgetDataContainer />
-                </div>
-            </Column>
-            {
-                two &&
-                <Column>
-                    <ProfileWrapper>
-                        <ProfileIcon />
-                    </ProfileWrapper>
+                    <SearchWrapper>
+                        <Search />
+                        {!two && <ProfileIcon />}
+                    </SearchWrapper>
                     <div>
                         <ProfileWidgetDataContainer />
-                        <Schedule />
                     </div>
                 </Column>
-            }
-        </Grid >
+                {
+                    two &&
+                    <Column>
+                        <ProfileWrapper>
+                            <ProfileIcon />
+                        </ProfileWrapper>
+                        <div>
+                            <ProfileWidgetDataContainer />
+                            <Schedule />
+                        </div>
+                    </Column>
+                }
+            </Grid >
+        </>
     );
 };
 
