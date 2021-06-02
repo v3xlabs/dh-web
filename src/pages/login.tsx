121cec73908753ef478e864f7572a95ce83f6034
DevEquinox <69062137+DevEquinox@users.noreply.github.com>
2021-06-02 12:09:49 -0700

    Fixed The Footer Code

--- src/pages/login.tsx
+++ src/pages/login.tsx
@@ -5,6 +5,7 @@ import parseURL from "url-parse";
 
 import { Button } from "../components/button/Button";
 import { Card } from "../components/card/Card";
+import { Logo } from "../components/logo/Logo";
 import SaferLink from "../library/SaferLink";
 
 const Wrapper = styled.div`
@@ -66,27 +67,9 @@ const Footer = styled.div`
 `;
 
 const Icon = styled.div`
-    border-radius: ${({ theme }) => theme.borderRadius};
-    height: 4rem;
-    width: 4rem;
-    max-width: 4rem;
-    max-height: 4rem;
+    
+    
     margin-left: 6rem;
-`;
-
-const IconText = styled.div`
-    color: ${({ theme }) => theme.palette.accent.default};
-    font-weight: 700;
-    margin-top: 6.1rem;
-    display: flex;
-    flex-direction: column;
-    margin-left: 2rem;
-    font-size: 2rem;
-    height: 100%;
-    span + span {
-        font-size: 1.4rem;
-        line-height: 1.4rem;
-    }
 `;
 
 const Text = styled.div`
@@ -184,39 +167,38 @@ const Login: FC = () => {
                 </Center>
             </Wrapper>
             <Footer>
-                <Icon>
-                    <img src="https://cdn.lvk.sh/dogehouse/logo.svg" alt="Dogehouse Revived" />
-                </Icon>
-                <IconText>
-                    <span>Dogehouse</span>
-                    <span>Revived</span>
-                </IconText>
+                
+                <Icon><Logo/></Icon>
+                
+               
                 <Text><a href="">Privacy policy</a></Text>
                 <Text><a href="https://github.com/dogehousetv/web/issues">Report a bug</a></Text>
                 
-                <DiscordIcon>   
-                    <svg
-                        width={20}
-                        height={20}
-                        viewBox="0 0 16 16"
-                        fill="currentColor"
-                        xmlns="http://www.w3.org/2000/svg"
-                    
-                    ><a href="https://discord.gg/gRKx5kcJtZ">
+                <DiscordIcon>  
+                    <SaferLink target="external" href="https://discord.gg/gRKx5kcJtZ"> 
+                        <svg
+                            width={20}
+                            height={20}
+                            viewBox="0 0 16 16"
+                            fill="currentColor"
+                            xmlns="http://www.w3.org/2000/svg"
+                        
+                        >
                             <path d="M6.3138 6.7124C5.85738 6.7124 5.4978 7.1124 5.4978 7.60082C5.4978 8.08756 5.8658 8.48924 6.3138 8.48924C6.76938 8.48924 7.12896 8.08756 7.12896 7.60082C7.13822 7.11156 6.76938 6.7124 6.3138 6.7124ZM9.23422 6.7124C8.7778 6.7124 8.41822 7.1124 8.41822 7.60082C8.41822 8.08756 8.78622 8.48924 9.23422 8.48924C9.6898 8.48924 10.0494 8.08756 10.0494 7.60082C10.0485 7.11156 9.6898 6.7124 9.23422 6.7124Z" />
                             <path d="M13.1224 0H2.40155C2.18568 0.000552227 1.97204 0.0436171 1.77282 0.126736C1.57359 0.209855 1.39269 0.331399 1.24044 0.48443C1.08819 0.637462 0.967579 0.818982 0.885482 1.01863C0.803385 1.21827 0.761415 1.43213 0.761968 1.648V12.464C0.761968 13.376 1.49797 14.112 2.40155 14.112H11.4735L11.0483 12.6307L12.0731 13.5832L13.0407 14.4792L14.762 16V1.648C14.7625 1.43213 14.7205 1.21827 14.6385 1.01863C14.5564 0.818982 14.4357 0.637462 14.2835 0.48443C14.1312 0.331399 13.9503 0.209855 13.7511 0.126736C13.5519 0.0436171 13.3383 0.000552227 13.1224 0V0ZM10.0335 10.448C10.0335 10.448 9.74555 10.1044 9.50639 9.79874C10.554 9.50232 10.954 8.84716 10.954 8.84716C10.6255 9.06274 10.314 9.216 10.0335 9.32042C9.01265 9.74768 7.88839 9.86427 6.80155 9.65558C6.39995 9.57669 6.00644 9.46112 5.62597 9.31032C5.42531 9.23409 5.23029 9.14376 5.04239 9.04C5.01797 9.02232 4.99439 9.01558 4.97081 8.99958C4.9582 8.99358 4.947 8.98497 4.93797 8.97432C4.79397 8.89516 4.71397 8.83958 4.71397 8.83958C4.71397 8.83958 5.09797 9.47958 6.11439 9.78358C5.87439 10.0867 5.57797 10.448 5.57797 10.448C3.81039 10.3916 3.13923 9.232 3.13923 9.232C3.13923 6.656 4.29123 4.56842 4.29123 4.56842C5.44323 3.70358 6.53881 3.728 6.53881 3.728L6.61881 3.824C5.17881 4.24084 4.51439 4.87242 4.51439 4.87242C4.51439 4.87242 4.69123 4.77642 4.98681 4.64084C5.84239 4.26526 6.52197 4.16084 6.80239 4.13642C6.85039 4.12884 6.89081 4.12042 6.93881 4.12042C8.32593 3.93349 9.73642 4.19711 10.9624 4.87242C10.9624 4.87242 10.33 4.27284 8.96997 3.85684L9.08197 3.72884C9.08197 3.72884 10.1784 3.70442 11.3295 4.56926C11.3295 4.56926 12.4815 6.65684 12.4815 9.23284C12.4815 9.232 11.802 10.3916 10.0335 10.448V10.448Z" />
-                        </a>
-                    </svg>
+                        </svg>
+                    </SaferLink>
                 </DiscordIcon>
                 <GithubIcon>
-                    <svg
-                        width={20}
-                        height={20}
-                        viewBox="0 0 16 16"
-                        fill="currentColor"
-                        xmlns="http://www.w3.org/2000/svg"
-                        
-                    ><a href="https://github.com/dogehousetv">
+                    <SaferLink target="external" href="https://github.com/dogehousetv">
+                        <svg
+                            width={20}
+                            height={20}
+                            viewBox="0 0 16 16"
+                            fill="currentColor"
+                            xmlns="http://www.w3.org/2000/svg"
+                            
+                        >
                             <g clipPath="url(#clip0)">
                                 <path
                                     fillRule="evenodd"
@@ -229,8 +211,8 @@ const Login: FC = () => {
                                     <rect width="16" height="16" />
                                 </clipPath>
                             </defs>
-                        </a>
-                    </svg>
+                        </svg>
+                    </SaferLink>
                 </GithubIcon>
             </Footer>
         </>
