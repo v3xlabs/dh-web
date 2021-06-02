472d4a88babd445241624e89b2752fedef1fefd6
Ankit Yadav <52023083+4nkitd@users.noreply.github.com>
2021-06-02 07:50:17 +0530

    Introduce `No Rooms` placeholder (#75)
    * Chalkedgoose/new modal pr (#71)
    
    * login page with redirect
    
    * Room Creation component logic done
    
    * nextjs config redirect
    
    * done
    
    * no room placeholder implementation
    
    Co-authored-by: Carlos A <soradev4@gmail.com>

---
+++ src/components/logo/NoRooms.tsx
@@ -0,0 +1,40 @@
+import React, { FC } from "react";
+import styled from "styled-components";
+
+const Wrapper = styled.div<{ small: boolean }>`
+    display: flex;
+    justify-content: ${({ small }) => small ? "center" : "flex-start"};
+    align-items: center;
+    flex: 1 100%;
+    padding: 10rem;
+    cursor: pointer;
+`;
+
+const Icon = styled.div`
+    border-radius: ${({ theme }) => theme.borderRadius};
+    height: auto;
+    flex: 1 100%;
+`;
+
+const Text = styled.div`
+    color: ${({ theme }) => theme.palette.primary[700]};
+    font-weight: 700;
+    flex: 1 100%;    
+    font-size: 3rem;
+    font-weight: 700;
+    margin-top: 40px;
+    text-align: center !important;
+`;
+
+type LogoProperties = Readonly<{
+    small?: boolean;
+}>
+
+export const NoRooms: FC<LogoProperties> = ({ small }: LogoProperties) => (
+    <Wrapper small={small}>
+        <Icon>
+            <img src="/no_rooms.svg" alt="No Rooms" width="100%" />
+            <Text>No Rooms, let&lsquo;s create one.</Text>
+        </Icon>
+    </Wrapper>
+);
