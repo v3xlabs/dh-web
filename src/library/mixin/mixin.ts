246539cf9ee4c8cb179dff7e59ec9ed83aac8d24
Carlos A <soradev4@gmail.com>
2021-06-05 10:06:22 -0700

    Created Skeleton Loader Mixin and Applied it to a few Places (#102)

--- src/library/mixin/mixin.ts
+++ src/library/mixin/mixin.ts
@@ -1,3 +1,5 @@
+import { css } from "styled-components";
+
 export const notDraggable = `
     user-drag: none; 
     user-select: none;
@@ -6,3 +8,41 @@ export const notDraggable = `
     -webkit-user-select: none;
     -ms-user-select: none;
 `;
+
+export const skeletonLoaderBase = css`
+box-sizing: border-box;
+position: relative;
+background: ${({ theme }) => theme.palette.primary[700]} no-repeat;
+animation: pulse 2s infinite;
+animation-delay: 0.5s;
+
+@media(prefers-reduced-motion: reduce) {
+    background-image: none;
+    animation: none;
+}
+  
+@keyframes pulse {
+    0% {
+      opacity: 1;
+    }
+    50% {
+      opacity: 0.4;
+    }
+    100% {
+      opacity: 1;
+    }
+  }
+`;
+
+/**
+ * Skeleton Loader Accessibility
+ * attributes.
+ */
+export const skeletonLoaderAttributes = {
+    "role": "progressbar",
+    "tabindex": 0,
+    "aria-busy": true,
+    "aria-valuemin": 0,
+    "aria-valuemax": 100,
+    "aria-valuetext": "Please wait..."
+};
