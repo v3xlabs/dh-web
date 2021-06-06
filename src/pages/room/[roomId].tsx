e27b31c6831116b3fdb462b5aa6e552d4fe16be6
Jonah <tgtechie01@gmail.com>
2021-06-06 18:13:16 -0400

    correct nesting for pages/room

--- src/pages/room/[roomId].tsx
+++ src/pages/room/[roomId].tsx
@@ -11,7 +11,7 @@ import { Logo } from "../../components/logo/Logo";
 import { Search } from "../../components/search/Search";
 import { ProfileIconDataContainer } from "../../components/user/ProfileIcon";
 import { withAuth } from "../../library/auth/withAuth";
-import useMediaQuery from "../library/hooks/useMediaQuery";
+import useMediaQuery from "../../library/hooks/useMediaQuery";
 
 const SearchWrapper = styled.div`
     width: 100%;
