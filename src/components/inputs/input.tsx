573ea9d152e97baac3bd17aaa1b8f649b79014c7
Joelimgu <joelimgu@gmail.com>
2021-06-11 19:33:19 +0200

    replacen input for our input component

--- src/components/inputs/input.tsx
+++ src/components/inputs/input.tsx
@@ -1,5 +1,42 @@
 import styled from "styled-components";
 
 // Use these in the event we want to modify them later
-export const TextArea = styled.textarea``;
-export const Input = styled.input``;
+export const TextArea = styled.textarea`
+    background: transparent;
+    border: none;
+    outline: none;
+    font-size: 100%;
+    color: inherit;
+    font-family: inherit;
+    line-height: inherit;
+    padding: 1rem 1rem;
+    box-shadow: rgb(0 0 0 / 10%) -3px 4px 14px;
+    border-color: ${({ theme }) => theme.palette.primary[700]};
+    background: ${({ theme }) => theme.palette.primary[700]};
+    border-radius: ${({ theme }) => theme.borderRadius};
+    flex: 1;
+
+    &:placeholder {
+        color: ${({ theme }) => theme.palette.primary[300]};
+    }
+`;
+
+export const Input = styled.input`
+    background: transparent;
+    border: none;
+    outline: none;
+    font-size: 100%;
+    color: inherit;
+    font-family: inherit;
+    line-height: inherit;
+    padding: 1rem 1rem;
+    box-shadow: rgb(0 0 0 / 10%) -3px 4px 14px;
+    border-color: ${({ theme }) => theme.palette.primary[700]};
+    background: ${({ theme }) => theme.palette.primary[700]};
+    border-radius: ${({ theme }) => theme.borderRadius};
+    flex: 1;
+
+    &:placeholder {
+        color: ${({ theme }) => theme.palette.primary[300]};
+    }
+`;
