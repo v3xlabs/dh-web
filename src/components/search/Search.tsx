408bac0481e54dcbdd5013443c2abf9b11c6da65
Joelimgu <joelimgu@gmail.com>
2021-06-13 01:10:48 +0200

    removed unnecessary curly braces

--- src/components/search/Search.tsx
+++ src/components/search/Search.tsx
@@ -64,7 +64,7 @@ export const Search: FC = () => {
     return (
         <Wrapper>
             <Box>
-                <SearchInput placeholder="Search for rooms, users or categories" onChange={(event) => {setSearchResults(event.target.value);}}/>
+                <SearchInput placeholder="Search for rooms, users or categories" onChange={(event) => setSearchResults(event.target.value)}/>
                 <Icon>
                     <svg width="16" height="16" viewBox="0 0 16 16" fill="currentColor" xmlns="http://www.w3.org/2000/svg"><g clipPath="url(#sm-solid-search_svg__clip0)" fillRule="evenodd" clipRule="evenodd"><path d="M7.212 1.803a5.409 5.409 0 100 10.818 5.409 5.409 0 000-10.818zM0 7.212a7.212 7.212 0 1114.424 0A7.212 7.212 0 010 7.212z"></path><path d="M11.03 11.03a.901.901 0 011.275 0l3.43 3.432a.902.902 0 01-1.274 1.275l-3.431-3.431a.901.901 0 010-1.275z"></path></g><defs><clipPath id="sm-solid-search_svg__clip0"><path d="M0 0h16v16H0z"></path></clipPath></defs></svg>
                 </Icon>
