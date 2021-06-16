24cd0fe13877362125a021e65fe10e208ce521cf
JoelImgu <44059689+joelimgu@users.noreply.github.com>
2021-06-16 18:17:17 +0200

    replaced all inputs with our component (#123)
    * replacen input for our input component
    
    * Revert "replacen input for our input component"
    
    This reverts commit 573ea9d152e97baac3bd17aaa1b8f649b79014c7.
    
    * replaced all inputs with our component
    
    * removed unnecessary curly braces
    
    * fixed indentation
    
    Co-authored-by: Carlos A <soradev4@gmail.com>

--- src/components/search/Search.tsx
+++ src/components/search/Search.tsx
@@ -1,6 +1,8 @@
 import React, { FC, useState } from "react";
 import styled from "styled-components";
 
+import { Input } from "../inputs/input";
+
 const Wrapper = styled.div`
     width: 100%;
     position: relative;
@@ -26,7 +28,7 @@ const Icon = styled.div`
     }
 `;
 
-const Input = styled.input`
+const SearchInput = styled(Input)`
     padding: 10px 20px 10px 0;
     flex: 1;
     color: ${({ theme }) => theme.palette.primary[100]};
@@ -62,7 +64,7 @@ export const Search: FC = () => {
     return (
         <Wrapper>
             <Box>
-                <Input placeholder="Search for rooms, users or categories" onChange={(event) => {setSearchResults(event.target.value);}}/>
+                <SearchInput placeholder="Search for rooms, users or categories" onChange={(event) => setSearchResults(event.target.value)}/>
                 <Icon>
                     <svg width="16" height="16" viewBox="0 0 16 16" fill="currentColor" xmlns="http://www.w3.org/2000/svg"><g clipPath="url(#sm-solid-search_svg__clip0)" fillRule="evenodd" clipRule="evenodd"><path d="M7.212 1.803a5.409 5.409 0 100 10.818 5.409 5.409 0 000-10.818zM0 7.212a7.212 7.212 0 1114.424 0A7.212 7.212 0 010 7.212z"></path><path d="M11.03 11.03a.901.901 0 011.275 0l3.43 3.432a.902.902 0 01-1.274 1.275l-3.431-3.431a.901.901 0 010-1.275z"></path></g><defs><clipPath id="sm-solid-search_svg__clip0"><path d="M0 0h16v16H0z"></path></clipPath></defs></svg>
                 </Icon>
